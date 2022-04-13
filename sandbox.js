6c94a64c77997d58b64fce39715809ef6b931856
Danila Fomin <dangdancheg@gmail.com>
2022-04-13 22:39:39 +0300

    object basics

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,9 @@
-const ul = document.querySelector(".people");
+let user = {
+  name: "kek",
+  puper: "stopper",
+  email: "some@milo.com",
+  blogs: ["1", "2", "3"],
+};
 
-const people = ["first", "second", "third", "fourth"];
-
-let html = ``;
-
-people.forEach(function (person) {
-  html += `<li style="color: purple">${person}</li>`;
-});
-
-console.log(people);
-ul.innerHTML = html;
+console.log(user);
+console.log(user.puper);
