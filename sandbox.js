ba863d9084c8e6d523c0573994699499c65ee70a
Danila Fomin <dangdancheg@gmail.com>
2022-04-14 19:57:38 +0300

    accessing math object

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,1 @@
-let user = {
-  name: "kek",
-  puper: "stopper",
-  email: "some@milo.com",
-  blogs: ["1", "2", "3"],
-  loginfo: function () {
-    console.log(this.email);
-  },
-};
-
-user.loginfo();
+console.log(Math.E);
